8f562effa0fd0b5930c13b7b5e3f05fca05a932a
Silviu Bogan <silviubogan@gmail.com>
2021-01-14 18:34:07 +0200

    Produce the actual critical.css file

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 
 "use strict";
 const generator = require("./critical");
+const fs = require('fs');
 const url = process.env.URL;
 const dimensions = process.env.DIMENSIONS || "1300x900";
 const location = process.env.LOCATION || "/output/critical.css";
 
 generator
   .generateCritical(url, dimensions, location)
-  .then((css) => {})
+  .then((css) => {
+    fs.writeFileSync(location, css);
+  })
   .catch((err) => {
     // console.error(err);
     console.error(err.name);
